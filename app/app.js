0f9e3c3eb8c45ac925b744ed1e3af960917a09a3
DilanUse <dilan_8-10@hotmail.com>
2022-04-30 15:54:11 -0500

    Installing TypeScript and Running the Compiler

--- app/app.js
+++ app/app.js
@@ -1,8 +1,6 @@
 function startGame() {
     // starting a new game
-    const messagesElements = document.getElementById('messages');
+    var messagesElements = document.getElementById('messages');
     messagesElements.innerText = 'Welcome to MultiMath! Starting new game...';
 }
-
 document.getElementById('startGame').addEventListener('click', startGame);
-

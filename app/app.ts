91da65535175ae94db311d35109f42961172bf37
DilanUse <dilan_8-10@hotmail.com>
2022-04-30 17:18:12 -0500

    Using let and const with Type Annotations

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 function startGame() {
     // starting a new game
+
+    const  playerName: string = 'Audrey';
+    logPlayer(playerName);
+
     const messagesElements = document.getElementById('messages');
     messagesElements!.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);

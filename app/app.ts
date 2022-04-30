fe8f5c278284390867d6a000ec2fb56a9a8e139d
DilanUse <dilan_8-10@hotmail.com>
2022-04-30 16:23:27 -0500

    Configuring Compiler Options in tsconfig.json

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,8 @@
 function startGame() {
     // starting a new game
     const messagesElements = document.getElementById('messages');
-    messagesElements.innerText = 'Welcome to MultiMath! Starting new game...';
+    messagesElements!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
-document.getElementById('startGame').addEventListener('click', startGame);
+document.getElementById('startGame')!.addEventListener('click', startGame);
 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+"use strict";
 function startGame() {
     // starting a new game
     var messagesElements = document.getElementById('messages');
     messagesElements.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
